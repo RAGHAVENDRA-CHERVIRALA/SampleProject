38fead6fc74ce53754bbd59d069e0f663291d54d
raghavendra-chervirala <raghavendrasourav@gmail.com>
2018-11-25 12:28:58 +0530

    secondcommit

---
+++ SampleProject/src/main/java/com/CommandLineArgument.java
@@ -0,0 +1,16 @@
+package com;
+
+public class CommandLineArgument {
+
+	public static void main(String[] args) {
+		
+	//	String name= args[0];
+	//	int empId= Integer.parseInt(args[1]); // Parsing String to Integer
+		
+	//	System.out.println("Name of Employ is:" +name);
+	//	System.out.println("Employ id is:"+empId);
+	 System.out.println("Welcome to Commandline arguments");
+
+	}
+
+}
